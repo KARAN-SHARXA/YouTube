16261e6ca6f6dd5a667f6a01933db57b6cb3d002
Karan Sharma <karansharma2302578@gmail.com>
2026-02-14 23:27:07 +0530

    video section and reco video

--- src/Components/recommented/Recommented.jsx
+++ src/Components/recommented/Recommented.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
-import './Recommented.css'
+import './Recommended.css'
 
-const Recommented = () => {
+import thumbnail1 from "../../assets/assets/thumbnail1.png";
+import thumbnail2 from "../../assets/assets/thumbnail2.png";
+import thumbnail3 from "../../assets/assets/thumbnail3.png";
+import thumbnail4 from "../../assets/assets/thumbnail4.png";
+import thumbnail5 from "../../assets/assets/thumbnail5.png";
+import thumbnail6 from "../../assets/assets/thumbnail6.png";
+import thumbnail7 from "../../assets/assets/thumbnail7.png";
+import thumbnail8 from "../../assets/assets/thumbnail8.png";
+
+const Recommended = () => {
+
+  const videos = [
+    thumbnail1, thumbnail2, thumbnail3, thumbnail4,
+    thumbnail5, thumbnail6, thumbnail7, thumbnail8
+  ];
+
   return (
-    <div>Recommented</div>
+    <div className="recommended">
+      {videos.map((thumb, index) => (
+        <div className="side-video-list" key={index}>
+          <img src={thumb} alt="" />
+          <div className="vid-info">
+            <h4>How to build a YouTube Clone with React</h4>
+            <p>Karan Stack</p>
+            <p>1.2M Views</p>
+          </div>
+        </div>
+      ))}
+    </div>
   )
 }
 
-export default Recommented
+export default Recommended
